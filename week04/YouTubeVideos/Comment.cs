c805b82f4f7e7d9a65b10fdad7698280e8da4331
Ulonna Godspower <108946078+GodspowerGP@users.noreply.github.com>
2026-05-29 10:16:50 +0100

    week 4 assignment

---
+++ week04/YouTubeVideos/Comment.cs
@@ -0,0 +1,7 @@
+using System;
+
+class Comment
+{
+    public string _name;
+    public string _text;
+}
--- week04/YouTubeVideos/Program.cs
+++ week04/YouTubeVideos/Program.cs
@@ -1,9 +1,91 @@
 using System;
+using System.Collections.Generic;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the YouTubeVideos Project.");
+        List<Video> videos = new List<Video>();
+
+        Video video1 = new Video();
+        video1._title = "How to learn C#";
+        video1._author = "Code Master";
+        video1._length = 600;
+
+        Comment v1Comment1 = new Comment();
+        v1Comment1._name = "John";
+        v1Comment1._text = "Great video!";
+        video1._comments.Add(v1Comment1);
+
+        Comment v1Comment2 = new Comment();
+        v1Comment2._name = "Mary";
+        v1Comment2._text = "Very helpful, thanks.";
+        video1._comments.Add(v1Comment2);
+
+        Comment v1Comment3 = new Comment();
+        v1Comment3._name = "Bob";
+        v1Comment3._text = "I still don't understand classes.";
+        video1._comments.Add(v1Comment3);
+
+        videos.Add(video1);
+
+        Video video2 = new Video();
+        video2._title = "Top 10 Programming Languages";
+        video2._author = "Tech Guru";
+        video2._length = 900;
+
+        Comment v2Comment1 = new Comment();
+        v2Comment1._name = "Alice";
+        v2Comment1._text = "Python is the best!";
+        video2._comments.Add(v2Comment1);
+
+        Comment v2Comment2 = new Comment();
+        v2Comment2._name = "Charlie";
+        v2Comment2._text = "What about Java?";
+        video2._comments.Add(v2Comment2);
+
+        Comment v2Comment3 = new Comment();
+        v2Comment3._name = "Dave";
+        v2Comment3._text = "Nice list.";
+        video2._comments.Add(v2Comment3);
+
+        videos.Add(video2);
+
+        Video video3 = new Video();
+        video3._title = "Funny Cats Compilation";
+        video3._author = "Cat Lover";
+        video3._length = 300;
+
+        Comment v3Comment1 = new Comment();
+        v3Comment1._name = "Eve";
+        v3Comment1._text = "So cute!";
+        video3._comments.Add(v3Comment1);
+
+        Comment v3Comment2 = new Comment();
+        v3Comment2._name = "Frank";
+        v3Comment2._text = "The second cat is hilarious.";
+        video3._comments.Add(v3Comment2);
+
+        Comment v3Comment3 = new Comment();
+        v3Comment3._name = "Grace";
+        v3Comment3._text = "I love cats.";
+        video3._comments.Add(v3Comment3);
+
+        videos.Add(video3);
+
+        foreach (Video video in videos)
+        {
+            Console.WriteLine($"Title: {video._title}");
+            Console.WriteLine($"Author: {video._author}");
+            Console.WriteLine($"Length: {video._length} seconds");
+            Console.WriteLine($"Number of comments: {video.GetNumberOfComments()}");
+            
+            foreach (Comment comment in video._comments)
+            {
+                Console.WriteLine($"- {comment._name}: {comment._text}");
+            }
+            
+            Console.WriteLine();
+        }
     }
 }
